7b30569a65a0cf31c990e9b5ece2c1b7b13519fe
hyagsyhy <hy.agsyhy@gmail.com>
2026-08-28 13:20:03 +0900

    secondcommit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Move : MonoBehaviour
+{
+    float playerSpeed = 3.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
