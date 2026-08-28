267a876ea15ce4e14e3c9ce95acead5e4c957294
hyagsyhy <hy.agsyhy@gmail.com>
2026-08-28 13:40:30 +0900

    secondcommit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -12,6 +12,18 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+        float h = 0f;
+
+        float v = 0f;
+        //キーボード入力を取得
+
+        if (UnityEngine.InputSystem.Keyboard.current != null)
+        {
+            if (UnityEngine.InputSystem.Keyboard.current.dKey.isPressed || UnityEngine.InputSystem.Keyboard.current.rightArrowKey.isPressed) h += 1f;
+            if (UnityEngine.InputSystem.Keyboard.current.aKey.isPressed || UnityEngine.InputSystem.Keyboard.current.leftArrowKey.isPressed) h -= 1f;
+            if (UnityEngine.InputSystem.Keyboard.current.wKey.isPressed || UnityEngine.InputSystem.Keyboard.current.upArrowKey.isPressed) v += 1f;
+            if (UnityEngine.InputSystem.Keyboard.current.sKey.isPressed || UnityEngine.InputSystem.Keyboard.current.downArrowKey.isPressed) v -= 1f;
+        }
     }
 }
