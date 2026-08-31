be71dc6a2e83c5335da20ebd4d9f80b1ab43d763
humankagoshimafurukawar <human.kagoshima.furukawar@gmail.com>
2026-08-31 17:22:41 +0900

    背景と障害物の動きを連動(仮)

--- Assets/Scripts/ObstaclesMover.cs
+++ Assets/Scripts/ObstaclesMover.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class ObstaclesMover : MonoBehaviour
 {
     GameObject gameController;
-
     GameController gameController_Script;
 
+    //------------------------------------------------------------------------------------------------------------------
+    GameObject bg_dummy_Center;
+    BGScroll bgScroll_Script;
+
+    //------------------------------------------------------------------------------------------------------------------
+    GameObject carController;
+    CarController carController_Script;
+
+    //------------------------------------------------------------------------------------------------------------------
     [SerializeField] float move_amount = 0;
 
     //======================================================================================================================
@@ -16,6 +24,14 @@ public class ObstaclesMover : MonoBehaviour
 
         // GameControllerが持っているスクリプトを取得
         gameController_Script = gameController.GetComponent<GameController>();
+
+        //------------------------------------------------------------------------------------------------------------------
+        bg_dummy_Center = GameObject.Find("bg_dummy_Center");
+        bgScroll_Script = bg_dummy_Center.GetComponent<BGScroll>();
+
+        //------------------------------------------------------------------------------------------------------------------
+        carController = GameObject.Find("CarController");
+        carController_Script = carController.GetComponent<CarController>();
     }
 
     //======================================================================================================================
@@ -23,7 +39,12 @@ public class ObstaclesMover : MonoBehaviour
     {
         float movement_Range = gameController_Script.screen_Size_x / 2.0f;
 
-        transform.Translate(-move_amount, 0, 0);
+        transform.Translate
+            (
+            -move_amount, // x軸移動量
+            bgScroll_Script.now_Speed * carController_Script.forward_or_back,  // y軸移動量
+            0 // z軸移動量
+            );
 
         if (transform.position.x < -movement_Range) { Destroy(this.gameObject); }
     }
