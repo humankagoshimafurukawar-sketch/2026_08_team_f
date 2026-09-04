c3e49324a48080109a4251b691e5e73818649233
humankagoshimafurukawar <human.kagoshima.furukawar@gmail.com>
2026-09-04 13:38:18 +0900

    残り距離算出機能を追加　前回のコミットにある「残り時間」は「経過時間」の間違いです

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,24 +1,68 @@
 using UnityEngine;
+using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] BGScroll bgScroll;
 
-    // 画面の横のサイズ
+    [Header("画面のサイズ")]
     public float screen_Size_x = 1280;
 
     public float screen_Size_y = 720;
 
+    //bool isPlaying;
+
+    [Header("ゴールまでの距離(km)")]
+    public float distance_to_goal = 0;
+
+    [Header("経過時間")]
     public float timer = 0;
+
+    // 秒速用
+    float per_Second_Speed = 0;
+
+    [Header("残りの距離")]
+    public float remaining_distance = 0;
+
+    // 残りの距離算出に使用するタイマー
+    float timer_for_remaining_distance = 0;
 
     //======================================================================================================================
     void Start()
     {
         Application.targetFrameRate = 60;
+
+        // ゴールまでの距離を残りの距離に写す
+        remaining_distance = distance_to_goal;
     }
 
     //======================================================================================================================
     void Update()
     {
-        timer += Time.deltaTime;
+        // 経過時間の計測
+        if (remaining_distance > 0)
+        { timer += Time.deltaTime; }
+
+        //------------------------------------------------------------------------------------------------------------------
+        const float per_hour_to_per_second = 3600;
+
+        // 秒速の算出
+        per_Second_Speed = bgScroll.now_Speed / per_hour_to_per_second;
+
+        //------------------------------------------------------------------------------------------------------------------
+        // 1秒をカウントする
+        timer_for_remaining_distance += Time.deltaTime;
+
+        // 残り距離算出 1秒ごとに残りの距離から秒速を引いていく
+        if (remaining_distance > 0 && timer_for_remaining_distance >= 1)
+        {
+            timer_for_remaining_distance = 0;
+            remaining_distance -= per_Second_Speed;
+        }
+
+        // 残り距離が0を下回ったら強制的に0にする
+        if(remaining_distance < 0) { remaining_distance = 0; }
+
+        Debug.Log(per_Second_Speed);
     }
 }
--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -8,6 +8,7 @@ public class TextManager : MonoBehaviour
 
     [SerializeField] TMP_Text now_Speed_Text;
     [SerializeField] TMP_Text timer_Text;
+    [SerializeField] TMP_Text distance_Text;
 
     void Start()
     {
@@ -16,12 +17,16 @@ public class TextManager : MonoBehaviour
 
     void Update()
     {
-        now_Speed_Text.text = bgScroll.now_Speed.ToString("F1") + ("km");
+        // 現在の速度を表示
+        now_Speed_Text.text = bgScroll.now_Speed.ToString("F1") + ("km/h");
 
         // timerをint型に変換し、分と秒を算出
         int min = Mathf.FloorToInt(gameController.timer / 60f);
         int sec = Mathf.FloorToInt(gameController.timer % 60f);
 
         timer_Text.text = $"{min:00}:{sec:00}";
+
+        // 残りの距離を表示
+        distance_Text.text = gameController.remaining_distance.ToString("F1") + ("km");
     }
 }
